ab736c06b3a655e00778a246166814f0ba8757a8
i-sshrq <i_ssh_rq@protonmail.com>
2020-03-27 10:11:11 +0600

    27-Mar-20-10:11:11.68

---
+++ Homework_1/problem_5.asm
@@ -0,0 +1,43 @@
+.model small
+org 100h
+.data
+.code
+ main proc
+    mov bh, 0
+    mov ah, 01h
+    int 21h
+    mov cl, al
+    sub cl, '0'
+    call newline
+    row:
+        inc bh
+        mov bl, cl
+        dec cl
+        spaces:
+            cmp cx, 00h
+            je next
+            mov dl, ' '
+            mov ah, 02h
+            int 21h
+            loop spaces
+        next:
+        mov cl, bh
+        stars:
+            mov dl, '*'
+            mov ah, 02h
+            int 21h
+            loop stars        
+        mov cl, bl
+        call newline
+        loop row   
+ endp
+ 
+ newline proc
+    mov dl, 10d
+    mov ah, 02h
+    int 21h
+    mov dl, 13d
+    int 21h
+    ret
+ endp
+end main
